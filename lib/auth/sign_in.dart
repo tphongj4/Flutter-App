cca2231319a840da21b22fdc54c9b44ef6dfeb61
Hien.HD <hienhdit@gmail.com>
2021-12-04 23:23:25 +0700

    Create Login Page Market Online by Hien.HD

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+// Tạo trang Login Page Market Online
+// Coder: Hien.HD - 04-12-2021
+
+class SignIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('images/background.png'),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Sign in to continue"),
+                  Text(
+                    "Market Online App",
+                    style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        shadows: [
+                          BoxShadow(
+                              blurRadius: 5,
+                              color: Colors.green.shade900,
+                              offset: Offset(3, 3))
+                        ]),
+                  ),
+                  Column(
+                    children: [
+                      SignInButton(
+                        Buttons.Apple,
+                        text: "Sign up with Apple",
+                        onPressed: () {},
+                      ),
+                      SignInButton(
+                        Buttons.Google,
+                        text: "Sign up with Google",
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "By signing in you are agreeing to you",
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                      Text(
+                        "Terms and Privacy Policy",
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_online_app/auth/sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,58 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Market Online App',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
-      home: const MyHomePage(title: 'Market Online App'),
-      debugShowCheckedModeBanner: false,
+      home: SignIn(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
