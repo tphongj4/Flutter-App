cac757208831ca54c7f266ccc0006320abb6a1ba
Hien.HD <hienhdit@gmail.com>
2021-12-05 22:09:40 +0700

    update code main and build.gradle by Hien.HD

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:market_online_app/screens_app/home_screen.dart';
 
 class SignIn extends StatefulWidget {
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:market_online_app/auth/sign_in.dart';
+import 'package:market_online_app/screens_app/home_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignIn(),
+      home: HomeScreen(),
     );
   }
 }
