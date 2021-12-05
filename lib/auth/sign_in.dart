3bda7dd76b5d9b0b003a2497b7362d6e57f4eb39
Phong Trương <ttphong.a5@gmail.com>
2021-12-05 10:40:59 +0700

    update sign-in Market online by TPhong

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,11 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 // Tạo trang Login Page Market Online
 // Coder: Hien.HD - 04-12-2021
 
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  // UserProvider userProvider;
+  Future<void> _googleSignUp() async {
+    try {
+      final GoogleSignIn _googleSignIn = GoogleSignIn(
+        scopes: ['email'],
+      );
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+
+      final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final User user = (await _auth.signInWithCredential(credential)).user;
+      print("signed in " + user.displayName);
+      // userProvider.addUserData(
+      //   currentUser: user,
+      //   userEmail: user.email,
+      //   userImage: user.photoURL,
+      //   userName: user.displayName,
+      // );
+      return user;
+    } catch (e) {
+      print(e.message);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
