71bd52cb570373a9827bf35fe35360915246189d
Nguyen Phuoc Le Hieu <hn0140902000@gmail.com>
2021-12-08 14:10:10 +0700

    create product_overview by Hieu.NPL final

---
+++ lib/screens_app/product_overview/product_overview.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:market_online_app/config/colors.dart';
+
+enum SigninCharacter { fill, outline }
+
+class ProductOverview extends StatefulWidget {
+  @override
+  _ProductOverviewState createState() => _ProductOverviewState();
+}
+
+class _ProductOverviewState extends State<ProductOverview> {
+  SigninCharacter _character = SigninCharacter.fill;
+
+  Widget buttonNavigatorBar({
+    Color iconColor,
+    Color backgroundColor,
+    Color color,
+    String tittle,
+    IconData iconData,
+  }) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        color: backgroundColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              iconData,
+              size: 17,
+              color: iconColor,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              tittle,
+              style: TextStyle(color: color),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Row(
+        children: [
+          buttonNavigatorBar(
+            backgroundColor: Colors.red,
+            color: Colors.white70,
+            iconColor: Colors.grey,
+            tittle: "Sản phẩm yêu thích",
+            iconData: Icons.favorite_outline,
+          ),
+          buttonNavigatorBar(
+            backgroundColor: Colors.green,
+            color: textColor,
+            iconColor: Colors.white70,
+            tittle: "Thêm giỏ hàng",
+            iconData: Icons.shop_outlined,
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        iconTheme: IconThemeData(color: textColor),
+        title: Text(
+          "Chi tiết nhu yếu phẩm",
+          style: TextStyle(color: textColor),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("Rau cải tươi xanh"),
+                    subtitle: Text("50k/1kg"),
+                  ),
+                  Container(
+                    height: 250,
+                    padding: EdgeInsets.all(40),
+                    child: Image.network(
+                        'https://khothietke.net/wp-content/uploads/2021/03/PNG00030-rau-cai-cai-thia-2-tai-png-mien-phi.png'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    width: double.infinity,
+                    child: Text(
+                      "Tuỳ chọn",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: textColor, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 3,
+                              backgroundColor: Colors.green[700],
+                            ),
+                            Radio(
+                              value: SigninCharacter.fill,
+                              groupValue: _character,
+                              activeColor: Colors.green[700],
+                              onChanged: (value) {
+                                setState(() {
+                                  _character = value;
+                                });
+                              },
+                            )
+                          ],
+                        ),
+                        Text("50k/1kg"),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add,
+                                size: 17,
+                                color: Colors.green,
+                              ),
+                              Text(
+                                "Thêm",
+                                style: TextStyle(color: Colors.green),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Về nhu yếu phẩm",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Cải ngọt có nguồn gốc từ Ấn Độ, Trung Quốc. Cây thảo, cao tới 50 - 100 cm, thân tròn, không lông, lá có phiến xoan ngược tròn dài, đầu tròn hay tù, gốc từ từ hẹp, mép nguyên không nhăn, mập, trắng trắng, gân bên 5 - 6 đôi, cuống dài, tròn.",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
